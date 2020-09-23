4586362be9a8041e219723c83048890b7611d8f4
tojslee <tojslee@yonsei.ac.kr>
2020-09-23 23:19:48 +0900

    200923 dns

--- project1/project.c
+++ project1/project.c
@@ -2,6 +2,9 @@
 #include <pcap.h>
 char errbuf[PCAP_ERRBUF_SIZE];
 pcap_if_t *allDevs;
+int network; // network variable
+int sniff; // sniff type variable 1:HTTP 2:DNS
+int no = 1;
 
 void parsing(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data);
 
@@ -24,7 +27,6 @@ int main(){
         if(i->description){printf(" (%s)", i->description);}
         printf("\n");
     }
-    int network;
     printf("Select Network: ");
     scanf("%d", &network);
     while(network <= 0 || network > counter){
@@ -33,7 +35,6 @@ int main(){
     }
 
     // choose whether to sniff
-    int sniff;
     printf("Choose whether to sniff.(1:HTTP, 2:DNS): ");
     scanf("%d", &sniff);
     while(sniff <= 0 || sniff > 2){
@@ -57,7 +58,13 @@ int main(){
 
     //pcal_loop(handle, 0, parsing, NULL);
 //
-    char *FILTER_RULE = "port 53";
+    char *FILTER_RULE;
+    if(sniff == 1){
+        FILTER_RULE = "port 80";
+    }
+    else if (sniff == 2){
+        FILTER_RULE = "port 53";
+    }
     struct bpf_program fcode;
     printf("1\n");
     if(pcap_compile(handle, &fcode, FILTER_RULE, 1, NULL) < 0){
@@ -77,14 +84,6 @@ int main(){
     pcap_loop(handle, 0, parsing, NULL);
     printf("4\n");
 
-    // packet sniffing
-    switch(sniff){
-    case 1:
-        break;
-    case 2:
-        break;
-    }
-
 
     pcap_freealldevs(allDevs);
     pcap_close(handle);
@@ -92,5 +91,72 @@ int main(){
 }
 
 void parsing(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data){
-    printf("please\n");
+    // pcap_pkthdr *header -> packet information
+    /* struct pcap_pkthdr{
+        struct timeval ts; pack capture time
+        bpf_u_int32 caplen; capture packet length
+        bpf_u_int32 len; real packet length
+    }
+*/
+    // u_char *pkt_data -> packet start address
+    if(sniff == 1){ // if packet of HTTP
+        
+    }
+    else if(sniff == 2){ // if pack of DNS
+        printf("\n");
+        // only header
+        // calculate port number
+        int s_port_f = *(pkt_data + 34);
+        int s_port_s = *(pkt_data + 35);
+        int d_port_f = *(pkt_data + 36);
+        int d_port_s = *(pkt_data + 37);
+        int s_port = s_port_f * 256 + s_port_s;
+        int d_port = d_port_f * 256 + d_port_s;
+
+        // print first line
+        printf("%d %d.%d.%d.%d:%d ", no++, *(pkt_data + 26), *(pkt_data + 27), *(pkt_data + 28), *(pkt_data + 29), s_port);
+        printf("%d.%d.%d.%d:%d DNS ID : %x%x\n", *(pkt_data + 30), *(pkt_data + 31), *(pkt_data + 32), *(pkt_data + 33), d_port, *(pkt_data + 42), *(pkt_data + 43));
+        
+        // calculate flag number from decimal to binary
+        int flag_f = *(pkt_data + 44);
+        int flag_s = *(pkt_data + 45);
+        int flag = flag_f * 256 + flag_s;
+        int binaryFormat[16];
+        for(int i=0;i<16;i++){
+            binaryFormat[16-i-1] = flag % 2;
+            flag = flag / 2;
+        }
+
+        // print flags
+        printf("%d | ", binaryFormat[0]);
+        printf("%d%d%d%d | ", binaryFormat[1], binaryFormat[2], binaryFormat[3], binaryFormat[4]);
+        printf("%d | %d | %d | %d | ", binaryFormat[5], binaryFormat[6], binaryFormat[7], binaryFormat[8]);
+        printf("%d%d%d | ", binaryFormat[9], binaryFormat[10], binaryFormat[11]);
+        printf("%d%d%d%d\n", binaryFormat[12], binaryFormat[13], binaryFormat[14], binaryFormat[15]);
+
+        // print others
+        printf("QDCOUNT : ");
+        if(*(pkt_data + 46)){
+            printf("%x%x\n", *(pkt_data + 46), *(pkt_data + 47));
+        }
+        else{printf("%x\n", *(pkt_data + 47));}
+
+        printf("ANCOUNT : ");
+        if(*(pkt_data + 48)){
+            printf("%x%x\n", *(pkt_data + 48), *(pkt_data + 49));
+        }
+        else{printf("%x\n", *(pkt_data + 49));}
+
+        printf("NSCOUNT : ");
+        if(*(pkt_data + 50)){
+            printf("%x%x\n", *(pkt_data + 50), *(pkt_data + 51));
+        }
+        else{printf("%x\n", *(pkt_data + 51));}
+
+        printf("ARCOUNT : ");
+        if(*(pkt_data + 52)){
+            printf("%x%x\n", *(pkt_data + 52), *(pkt_data + 53));
+        }
+        else{printf("%x\n", *(pkt_data + 53));}
+    }
 }
