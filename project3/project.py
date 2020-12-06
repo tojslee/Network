384d60dd5fc7f2143962651c77e14332b1ceeea6
Lee Jisoo <54274442+tojslee@users.noreply.github.com>
2020-12-06 16:42:20 +0900

    Update project.py

--- project3/project.py
+++ project3/project.py
@@ -6,35 +6,22 @@ clients = []
 clientAddrs = []
 clientThreads = []
 clientNums = 0
+printNums = 1
 
 lock = threading.Lock()
 
 def printBar():
     print("----------------------------------------------------\n");
 
-
-def newClient(serverSocket):
-    global clients, clientAddrs, clientNums, clientThreads
-
+def newClient():
     while True:
+        # get data
         clientSocket, addr = serverSocket.accept()
+        data = clientSocket.recv(8192)
 
-        # handle information of the new client
-        lock.acquire()
-        clients.append(clientSocket)
-        clientAddrs.append(addr)
-        clientNums += 1
-
-        newThread = threading.Thread(target=receiver, args=(clientSocket,addr))
-        newThread.daemon = True
-        newThread.start()
-        clientThreads.append(newThread)
-        lock.release()
-
-def receiver(clientSocket, addr):
-    # handle request and sent request to server
-    data = clientSocket.recv(8192)
-    print(data)
+        # if the request from the client is GET
+        request_First_line = data.split("\n")[0]
+        print(request_First_line)
 
 # get argv value Port number
 serverPort = int(sys.argv[1])
@@ -45,15 +32,15 @@ serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind(('', serverPort))
 serverSocket.listen()
 
-print("Starting proxy server on port", serverPort);
-printBar();
+print("Starting proxy server on port", serverPort)
+printBar()
 
 # get client request
 while True:
     # except keyboard interrupt
     try:
         # new thread for handling new client
-        setClient = threading.Thread(target=newClient, args=(serverSocket, ))
+        setClient = threading.Thread(target=newClient, args=())
         setClient.daemon = True
         setClient.start()
         setClient.join()
